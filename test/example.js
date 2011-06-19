056932dd5b4bf8459d1f0a0e02c93b82d016ae5c
e6nian <e6nian@gmail.com>
2011-06-19 15:16:16 +0800

    basic working release with example

---
+++ test/example.js
@@ -0,0 +1,18 @@
+var top = require('../lib/node-taobao')
+    ,options = {'host':'gw.api.tbsandbox.com','appKey':'test','appSecret':'test'}
+    ,params  = {'method':'taobao.user.get','fields':'user_id,uid,sex','nick':'sandbox_c_1'}
+    ;
+
+
+var client = top.createClient(options);
+var result = client.getRequest(params,function(err,data){
+  if(!err){
+    console.log(data);
+    console.log('getRequest ok');
+  }else{
+    console.log(err);
+    console.log('getRequest err occurs');
+  }
+});
+
+
